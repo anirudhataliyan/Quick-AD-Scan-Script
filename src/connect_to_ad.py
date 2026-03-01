04ac6101520d9489300384c36475af56a3255b33
Anirudha Taliyan <akt746083@gmail.com>
2026-03-01 11:00:00 +0530

    Update connect_to_ad.py

--- src/connect_to_ad.py
+++ src/connect_to_ad.py
@@ -1,6 +1,7 @@
 import ldap3
 
+
 def connect_to_ad(server_address, username, password):
     server = ldap3.Server(server_address, get_info=ldap3.ALL)
-    conn = ldap3.Connection(server, user=username, password=password, auto_bind=True)
+    conn   = ldap3.Connection(server, user=username, password=password, auto_bind=True)
     return conn
